3ea1f4f19a5aa4338bca04ad23b0c139ce719081
Bhuwanesh16 <bhuwaneshmanivel@gmail.com>
2024-12-30 15:16:25 +0530

    product

--- projectportfolio/src/App.js
+++ projectportfolio/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
 import Login from './components/header/Login';
+import Product from './components/products/Product';
 function App() {
   return (
     <div>
-      <Login/>
-      <Header/>
+      
+      <Product/>
     </div>
   );
 }
